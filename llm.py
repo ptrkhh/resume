7b54057d7465348e7682234cb7a4c116fef55449
Patrick Hermawan <patrick.hermawan@outlook.com>
2025-03-23 01:46:39 +0700

    update base prompt

--- llm.py
+++ llm.py
@@ -10,12 +10,16 @@ def initialize_llm(profile):
     template = f"""You are an AI assistant for Patrick in his job search. 
     Please give convincing answers why Patrick is a good candidate and should be hired for this job.
     Keep your answers to 3 or fewer sentences unless absolutely necessary.
-    If you don't know the answer, inform the recruiter to contact Patrick. 
+    
     Here's Patrick's information:
     \n\n====\n\n
     {yaml.dump(profile)}
     \n\n====\n\n
-    Here is the chat history, use this to understand what to say next: {{memory}}
+    If there's no relevant answer in the information above, kindly inform the recruiter to contact Patrick.
+    
+    Here is the chat history, use this to understand what to say next: 
+    
+    {{memory}}
     Human: {{human}}
     AI:
     """
